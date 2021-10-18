3453d0bcba30ef89634959b1dc0a88fb97911a14
CollieIsCute <qetup1988@gmail.com>
2021-10-19 02:48:46 +0800

    update .zshrc to be more flexible and add setup.zsh for zsh

---
+++ zsh_settings/setup.sh
@@ -0,0 +1,9 @@
+#!bash
+# install ohmyzsh
+sh -c "$(wget -O- https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+# add theme of zsh
+cp ./agnosterzak.zsh-theme ~/.oh-my-zsh/themes/
+# install autosuggesstions
+git clone https://github.com/zsh-users/zsh-autosuggestions ~/.zsh/zsh-autosuggestions
+# install you-should-use
+git clone https://github.com/MichaelAquilina/zsh-you-should-use.git $ZSH_CUSTOM/plugins/you-should-use
