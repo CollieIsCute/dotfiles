e411b5a1296adf433d5057035d69f9698f19f530
CollieIsCute <qetup1988@gmail.com>
2021-10-24 22:31:32 +0800

    update setup.sh in zsh_settings

--- zsh_settings/setup.sh
+++ zsh_settings/setup.sh
@@ -1,14 +1,14 @@
-#!bash
+#!zsh
 # install ohmyzsh
 sh -c "$(wget -O- https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 # add theme of zsh
 cp ./agnosterzak.zsh-theme $ZSH/themes/
 # install autosuggestions
-git clone https://github.com/zsh-users/zsh-autosuggestions $ZSH_CUSTOM/plugins/zsh-autosuggestions
+git clone https://github.com/zsh-users/zsh-autosuggestions "$ZSH_CUSTOM/plugins/zsh-autosuggestions"
 # install you-should-use
-git clone https://github.com/MichaelAquilina/zsh-you-should-use.git $ZSH_CUSTOM/plugins/you-should-use
+git clone https://github.com/MichaelAquilina/zsh-you-should-use.git "$ZSH_CUSTOM/plugins/you-should-use"
 # install zsh-syntax-highlighting
-git clone https://github.com/zsh-users/zsh-syntax-highlighting.git $ZSH_CUSTOM/plugins/zsh-syntax-highlighting
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git "$ZSH_CUSTOM/plugins/zsh-syntax-highlighting"
 # set .zshrc
 ln -sf `pwd -P`/.zshrc ~/.zshrc
 # set .tmux.conf
