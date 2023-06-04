6f2467164a980a2a3644912a0da5cce191345b6c
Collie Tsai <qetup1988+github@gmail.com>
2023-06-04 08:19:37 +0800

    Add goto definition

---
+++ home/.config/nvim/lua/lsp/baseLsp.lua
@@ -0,0 +1,3 @@
+print("baseLsp is called")
+vim.api.nvim_set_keymap("n", "gD", "<cmd>lua vim.lsp.buf.declaration()<CR>", { noremap = true, silent = true })
+vim.api.nvim_set_keymap("n", "gd", "<cmd>lua vim.lsp.buf.definition()<CR>", { noremap = true, silent = true })
--- home/.config/nvim/lua/lsp/cpp.lua
+++ home/.config/nvim/lua/lsp/cpp.lua
@@ -1,3 +1,5 @@
+print("cpp.lua is called")
+require('lsp/baselsp')
 require('lspconfig').clangd.setup{
 	on_attach = on_attach,
 	flags = {
