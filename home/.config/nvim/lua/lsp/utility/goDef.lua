bf8ce47e6750f5cb970fe76fff0fc294cf5fb398
Collie Tsai <qetup1988+github@gmail.com>
2023-06-17 00:53:02 +0800

    add hover

--- home/.config/nvim/lua/lsp/utility/goDef.lua
+++ home/.config/nvim/lua/lsp/utility/goDef.lua
@@ -1,3 +1,4 @@
 print("baseLsp is called")
 vim.api.nvim_set_keymap("n", "gD", "<cmd>lua vim.lsp.buf.declaration()<CR>", { noremap = true, silent = true })
 vim.api.nvim_set_keymap("n", "gd", "<cmd>lua vim.lsp.buf.definition()<CR>", { noremap = true, silent = true })
+vim.keymap.set('n', 'K', vim.lsp.buf.hover, {noremap = true, silent = true})
