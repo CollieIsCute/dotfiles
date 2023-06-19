5b6552f38fe1818ff382d2e116b97c81d627aad5
Collie Tsai <qetup1988+github@gmail.com>
2023-06-20 03:10:31 +0800

    add rename and other functions

--- home/.config/nvim/lua/lsp/utility/goDef.lua
+++ home/.config/nvim/lua/lsp/utility/goDef.lua
@@ -1,10 +1,11 @@
 print("baseLsp is called")
-local bufopts = {noremap=true, silent=true}
+local bufopts = { noremap = true, silent = true }
 vim.keymap.set('n', 'K', vim.lsp.buf.hover, bufopts)
 vim.keymap.set('n', 'gd', vim.lsp.buf.definition, bufopts)
 vim.keymap.set('n', 'gi', vim.lsp.buf.implementation, bufopts)
 vim.keymap.set('n', 'gr', vim.lsp.buf.references, bufopts)
 vim.keymap.set('n', 'gD', vim.lsp.buf.declaration, bufopts)
-vim.keymap.set('n', '<space>K', vim.lsp.buf.signature_help, bufopts)
 vim.keymap.set('n', '[d', vim.diagnostic.goto_prev, bufopts)
 vim.keymap.set('n', ']d', vim.diagnostic.goto_next, bufopts)
+vim.keymap.set('n', 'gt', vim.lsp.buf.type_definition, bufopts)
+vim.keymap.set('n', '<Leader>rn', vim.lsp.buf.rename, bufopts)
