0a8b909c4fc6eb94836ba419793ca73e5ae13347
collieiscute <colliemst@gmail.com>
2023-07-23 02:25:42 +0800

    add ubuntu version package script

---
+++ setup/configuration/ubuntu.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/bash
+DOTFILE_PATH="`pwd -P`/.."
+GREEN='\033[1;32m'
+NC='\033[0m'
+
+echo -e "${GREEN}Setting up configurations...${NC}"
+echo -e "Copying following files to ~/:"
+for f in ${DOTFILE_PATH}/home/.[!.g]*
+do
+	echo ${f}
+done
+cp -lR --remove-destination ${DOTFILE_PATH}/home/.[!.g]* ~
+
+echo -e "${GREEN}Configurating zsh and tmux, and this may need user password...${NC}"
+chsh -s $(which zsh)
+echo -e "${GREEN}Installing zsh plugins...${NC}"
+zsh -c 'git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions'
+zsh -c 'git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting'
+zsh -c 'git clone https://github.com/MichaelAquilina/zsh-you-should-use.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/you-should-use'
+
---
+++ setup/install_package/ubuntu.sh
@@ -0,0 +1,38 @@
+#!/usr/bin/bash
+GREEN='\033[1;32m'
+NC='\033[0m'
+
+apt_packages=(
+	"bat"
+	"build-essential"
+	"clang-format"
+	"curl"
+	"git"
+	"htop"
+	"ibus-chewing"
+	"neofetch"
+	"ssh"
+	"tig"
+	"tmux"
+	"wget"
+	"zsh"
+)
+snap_packages=(
+	"nvim --classic"
+	"telegram-desktop"
+)
+
+echo -e "${GREEN}Installing packages...${NC}"
+apt update && apt upgrade -y
+apt install "${apt_packages[@]}" -y
+
+# install brave
+curl -fsSLo /usr/share/keyrings/brave-browser-archive-keyring.gpg https://brave-browser-apt-release.s3.brave.com/brave-browser-archive-keyring.gpg
+echo "deb [signed-by=/usr/share/keyrings/brave-browser-archive-keyring.gpg arch=amd64] https://brave-browser-apt-release.s3.brave.com/ stable main"|tee /etc/apt/sources.list.d/brave-browser-release.list
+apt update && apt install brave-browser
+
+apt autoremove
+for snap_pkg in "${snap_packages[@]}"
+do
+	snap install ${snap_pkg}
+done
