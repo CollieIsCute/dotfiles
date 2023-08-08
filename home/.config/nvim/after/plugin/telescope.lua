c11eeba78b410cb1457e8cfe28d6357495161e5d
collie <colliemst@gmail.com>
2023-08-08 14:30:48 +0800

    make telescope cannot see build dir

--- home/.config/nvim/after/plugin/telescope.lua
+++ home/.config/nvim/after/plugin/telescope.lua
@@ -1,4 +1,13 @@
+local telescope = require('telescope')
 local builtin = require('telescope.builtin')
+telescope.setup({
+    defaults = {
+        file_ignore_patterns = {
+            "build",
+            "log"
+        }
+	},
+})
 vim.keymap.set('n', '<leader>pf', builtin.find_files, {})
 vim.keymap.set('n', '<C-p>', builtin.git_files, {})
 vim.keymap.set('n', '<leader>ps', function()
