c19a4f9e54edf5d8988de82b1177c0c2b6c95b4f
collieiscute <colliemst@gmail.com>
2023-11-21 02:31:03 +0800

    able to use most part of cmp (except lsp part)

--- home/.config/nvim/lua/CollieIsCute/lazy_nvim.lua
+++ home/.config/nvim/lua/CollieIsCute/lazy_nvim.lua
@@ -70,7 +70,15 @@ require("lazy").setup({
             'hrsh7th/cmp-nvim-lsp',
             'hrsh7th/cmp-buffer',
             'hrsh7th/cmp-path',
+            'hrsh7th/cmp-cmdline',
             'saadparwaiz1/cmp_luasnip'
         }
+    },
+    {
+	    "L3MON4D3/LuaSnip",
+	    -- follow latest release.
+	    version = "v2.*", -- Replace <CurrentMajor> by the latest released major (first number of latest release)
+	    -- install jsregexp (optional!).
+	    build = "make install_jsregexp"
     }
 })
