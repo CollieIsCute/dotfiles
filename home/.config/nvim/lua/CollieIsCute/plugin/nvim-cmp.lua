366e562c71806d4fffbb6bba4b55c2ee6fae30db
collieiscute <colliemst@gmail.com>
2023-11-26 02:04:44 +0800

    update cmp with cmdline and search

--- home/.config/nvim/lua/CollieIsCute/plugin/nvim-cmp.lua
+++ home/.config/nvim/lua/CollieIsCute/plugin/nvim-cmp.lua
@@ -75,3 +75,26 @@ cmp.setup({
         native_menu = false,
     },
 })
+
+-- `/` cmdline setup.
+cmp.setup.cmdline('/', {
+  mapping = cmp.mapping.preset.cmdline(),
+  sources = {
+    { name = 'buffer' }
+  }
+})
+
+-- `:` cmdline setup.
+cmp.setup.cmdline(':', {
+  mapping = cmp.mapping.preset.cmdline(),
+  sources = cmp.config.sources({
+    { name = 'path' }
+  }, {
+    {
+      name = 'cmdline',
+      option = {
+        ignore_cmds = { 'Man', '!' }
+      }
+    }
+  })
+})
--- home/.config/nvim/lua/lsp/lua.lua
+++ home/.config/nvim/lua/lsp/lua.lua
@@ -1,5 +1,7 @@
 require('lsp.utility.goDef')
+local capabilities = require('cmp_nvim_lsp').default_capabilities()
 require('lspconfig').lua_ls.setup{
+    capabilities = capabilities,
 	flags = {
 		debounce_text_changes = 150,
 	},
