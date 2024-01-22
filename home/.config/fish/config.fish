6e4bdcc97dd092c36b585bd6c0eb33909e01c504
collie mst <colliemst@gmail.com>
2024-01-23 03:33:22 +0800

    fix environment variable issue

--- home/.config/fish/config.fish
+++ home/.config/fish/config.fish
@@ -1,3 +1,11 @@
+# run OS specific script
+switch (uname)
+case Darwin
+  source (stauts dirname)/config-macos.fish
+case Linux
+  source (stauts dirname)/config-linux.fish
+end
+
 # install omf if not installed
 if not type -q omf
     curl https://raw.githubusercontent.com/oh-my-fish/oh-my-fish/master/bin/install | fish
@@ -26,12 +34,4 @@ if cat /etc/os-release | grep -q -E "Ubuntu"
     alias bat="batcat --pager='never'" 
 else
     alias bat="bat --pager='never''"
-end
-
-# run OS specific script
-switch (uname)
-  case Darwin
-    source (stauts dirname)/config-macos.fish
-  case Linux
-    source (stauts dirname)/config-linux.fish
 end
