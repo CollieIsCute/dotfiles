020753d05598bb3d27a55bb2e3e01da5d0d04121
牧羊犬真Q <colliemst@gmail.com>
2024-07-19 00:36:35 +0800

    Feature/copilot with cpp (#5)
    * Fix: copilot can work with clangd and c++
    
    * Add: lua to treesitter
    
    * Update fish alias

--- home/.config/fish/config.fish
+++ home/.config/fish/config.fish
@@ -1,27 +1,27 @@
 # run OS specific script
 switch (uname)
-case Darwin
-  source (dirname (status --current-filename))/config-macos.fish
-case Linux
-  source (dirname (status --current-filename))/config-linux.fish
+    case Darwin
+        source (dirname (status --current-filename))/config-macos.fish
+    case Linux
+        source (dirname (status --current-filename))/config-linux.fish
 end
 
 # user define alias
-alias gaa "git add --all"
-alias gc "git commit -m"
-alias gf "git fetch"
-alias gl "git pull"
-alias glg "git log"
-alias gp "git push"
-alias gst "git status"
-alias la "ls -A"
-alias lal "la -lh"
-alias ll "ls -lh"
-alias sdn "shutdown now"
-alias tosync "cd ~/syncfolder/"
-alias ... "cd ../.."
-alias .... "cd ../../.."
-alias ..... "cd ../../../.."
-alias ...... "cd ../../../../.."
-alias ....... "cd ../../../../../.."
-command -qv nvim && alias vi nvim
+alias gaa="git add --all"
+alias gc="git commit -m"
+alias gf="git fetch"
+alias gl="git pull"
+alias glg="git log"
+alias gp="git push"
+alias gst="git status"
+alias la="ls -A"
+alias lal="la -lh"
+alias ll="ls -lh"
+alias sdn="shutdown now"
+alias tosync="cd ~/syncfolder/"
+alias ...="cd ../.."
+alias ....="cd ../../.."
+alias .....="cd ../../../.."
+alias ......="cd ../../../../.."
+alias .......="cd ../../../../../.."
+command -v nvim >/dev/null && alias vi="nvim"
