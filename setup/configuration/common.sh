9d09db7b82db91a216fc7bbdf49d28fff879c514
collie <colliemst@gmail.com>
2024-07-19 00:13:07 +0800

    Fix: copilot can work with clangd and c++

--- setup/configuration/common.sh
+++ setup/configuration/common.sh
@@ -1,15 +1,14 @@
+#!bash
 echo -e "${GREEN}Setting up configurations...${NC}"
 echo -e "Copying following files to ~/:"
 for f in ${DOTFILE_PATH}/home/.[!.g]*; do
-	echo ${f}
+  echo ${f}
 done
 
 # install LazyVim
 # required
-mv ~/.config/nvim{,.bak}
-mv ~/.local/share/nvim{,.bak}
-mv ~/.local/state/nvim{,.bak}
-mv ~/.cache/nvim{,.bak}
+rm -rf ~/.config/nvim ~/.local/share/nvim ~/.local/state/nvim ~/.cache/nvim
 git clone https://github.com/LazyVim/starter ~/.config/nvim
 rm -rf ~/.config/nvim/.git
 cp -lR --remove-destination ${DOTFILE_PATH}/nvim/lua/* ~/.config/nvim/lua/
+
