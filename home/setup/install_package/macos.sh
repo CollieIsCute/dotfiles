3a9e70c060f1a38f230350ee1c8be00ed0ab334a
collie mst <colliemst@gmail.com>
2025-01-05 16:32:10 +0800

    remove oh my fish, and add some warning when trying to install aur packages because not support yet

--- home/setup/install_package/macos.sh
+++ home/setup/install_package/macos.sh
@@ -11,14 +11,3 @@ brew bundle install --file=install_package/Brewfile
 # add brew path to fish
 (fish -C "eval (/opt/homebrew/bin/brew shellenv) && exit")
 
-# check & install oh-my-fish
-if [ -z "$OMF_PATH" ]; then
-    echo "Installing oh-my-fish..."
-
-    # Install oh-my-fish and use EOF to force quit fish shell
-    curl https://raw.githubusercontent.com/oh-my-fish/oh-my-fish/master/bin/install > install
-    fish install --path=~/.local/share/omf --config=~/.config/omf
-    rm -rf install
-    curl https://raw.githubusercontent.com/oh-my-fish/oh-my-fish/master/bin/install | fish
-
-fi
