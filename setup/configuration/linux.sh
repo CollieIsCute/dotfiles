c1ed459f29987ee0416d53904127f0a22f02e135
牧羊犬真Q <colliemst@gmail.com>
2025-01-14 22:13:47 +0800

    Introduce chezmoi to dotfiles (#6)
    * update dockerfile and rename files with chezmoi syntax
    
    * update dockerfile
    
    * move applying action to runtime instead of dockerfile
    
    * remove redundant comment
    
    * move all packages installation to chezmoi template
    
    * move fish installation to chezmoi template
    
    * add arch/manjaro version chezmoi template
    
    * fix syntax error
    
    * auto select pacman mirror and fix syntax issue
    
    * remove teams as dependencies
    
    * fix syntax error
    
    * retry when network is not good
    
    * fix retry with double quote
    
    * use chezmoi api to distinguish os distro
    
    * fix local variable issue
    
    * combine two case
    
    * fix syntax error
    
    * use chezmoi to copy files instead
    
    * remove password in docker container
    
    * split command to different layer
    
    * remove chsh part in install script
    
    * update readme
    
    * partially update readme
    
    * remove unused install script
    
    * add packages information
    
    * add chezmoi metadata
    
    * rearrange dotfiles
    
    * split run_once_install into two, one for setup, and one for install packages everytime I update scripts
    
    * move installation of chezmoi to entrypoint
    
    * fix syntax issues
    
    * fix  is not bash var issue
    
    * move paru to run_once script
    
    * install git before install paru
    
    * fix syntax chezmoidata issue
    
    * install all packages at once
    
    * fix syntax issues
    
    * fix return type issue
    
    * remove dulplicated installation of paru
    
    * remove unused script
    
    * migrate lazyvim installation to chezmoi system
    
    * remove oh my fish, and add some warning when trying to install aur packages because not support yet
    
    * fix paru compile issues
    
    * add some packages to auto install
    
    * add nvm to fisher plugins
    
    * remove steam as default package
    
    * use paru instead, and add some packages
    
    * add paru configs
    
    * add multilib for steam and may some other 32bit applications
    
    * split different function into files
    
    * add prefix for make sure it is first executed
    
    * remove redundant file

--- setup/configuration/linux.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/bash
-cp -lR --remove-destination ${DOTFILE_PATH}/home/.[!.]* ~
-source ${DOTFILE_PATH}/setup/configuration/common.sh
-# switch shell to fish
-echo -e "${GREEN}Switching shell to fish...${NC}"
-chsh -s $(which fish)
