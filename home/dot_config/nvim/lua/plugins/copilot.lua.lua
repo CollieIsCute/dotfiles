db0923ac69bc3444529ace66462e80055d777c8e
Collie Tsai <colliemst@gmail.com>
2025-02-06 04:40:18 +0800

    update copilot.lua.lua and copilot.cmp.lua to new version

--- home/dot_config/nvim/lua/plugins/copilot.lua.lua
+++ home/dot_config/nvim/lua/plugins/copilot.lua.lua
@@ -1,18 +1,15 @@
 return {
-  "zbirenbaum/copilot.lua",
-  cmd = "Copilot",
-  build = ":Copilot auth",
-  config = function()
-    require("copilot").setup({})
-  end,
-  opts = {
-    suggestion = { enabled = false },
-    panel = { enabled = false },
-    filetypes = {
-      markdown = true,
-      help = true,
-      cpp = true,
-      ["*"] = true,
-    },
-  },
+  {
+    "zbirenbaum/copilot.lua",
+    opts = function()
+      LazyVim.cmp.actions.ai_accept = function()
+        if require("copilot.suggestion").is_visible() then
+          LazyVim.create_undo()
+          require("copilot.suggestion").accept()
+          return true
+        end
+      end
+    end,
+  }
 }
+
