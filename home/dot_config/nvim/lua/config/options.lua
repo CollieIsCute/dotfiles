e3acfafb3cc9e5cd35836d4ef5c696fb546cc292
Collie Tsai <colliemst@gmail.com>
2025-02-10 01:48:23 +0800

    add fzf options

---
+++ home/dot_config/nvim/lua/config/options.lua
@@ -0,0 +1,7 @@
+-- Options are automatically loaded before lazy.nvim startup
+-- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
+-- Add any additional options here
+
+-- In case you don't want to use `:LazyExtras`,
+-- then you need to set the option below.
+vim.g.lazyvim_picker = "fzf"
