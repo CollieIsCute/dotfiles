2ca2c33570e714dcb061994808bdf0edb9b73bc2
Collie Tsai <colliemst@gmail.com>
2025-08-15 03:23:09 +0800

    setup fish command wrap and completions

--- home/dot_config/fish/functions/wo.fish
+++ home/dot_config/fish/functions/wo.fish
@@ -1,19 +1,27 @@
-# ~/.config/fish/functions/wo.fish
-function wo --description 'Open file/URL with the system opener'
-    set -l code 127
-
-    for item in $argv
-        if type -q open
-            # macOS: open 一次可收多個參數；逐一處理也可
-            command open -- "$item" >/dev/null 2>&1 &
-            set code 0
-        else if type -q xdg-open
-            # Linux: xdg-open 通常一次處理一個，逐一呼叫較穩
-            command xdg-open -- "$item" >/dev/null 2>&1 &
-            set code 0
-        end
+function wo --description 'Open file/URL with the system opener' --wraps open
+    set -l opener
+    if command -q open
+        set opener open
+    else if command -q xdg-open
+        set opener xdg-open
+    else if command -q gio
+        set opener "gio open"
+    else
+        echo "wo: no opener (open/xdg-open/gio) found" 1>&2
+        return 127
     end
 
-    test $code -eq 0; and disown
-    return $code
+    set -l rc 0
+    for item in $argv
+        if test "$opener" = "gio open"
+            command gio open -- "$item" >/dev/null 2>&1 &
+        else
+            command $opener -- "$item" >/dev/null 2>&1 &
+        end
+        set -l s $status
+        test $s -eq 0; or set rc $s
+    end
+
+    jobs -q; and disown
+    return $rc
 end
