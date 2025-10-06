5212d2f39efb4abc83ab57346374ee9e4e7b3ec3
Collie Tsai <colliemst@gmail.com>
2025-10-07 01:20:45 +0800

    fix lazyvim smear cursor upgrade breaking issue

--- home/dot_config/nvim/lua/plugins/smear-cursor.nvim.lua
+++ home/dot_config/nvim/lua/plugins/smear-cursor.nvim.lua
@@ -1,21 +1,7 @@
 return {
-	{
-		"sphamba/smear-cursor.nvim",
-		event = "VeryLazy",
-		cond = vim.g.neovide == nil,
-		opts = {
-			hide_target_hack = true,
-			cursor_color = "none",
-		},
-		specs = {
-			-- disable mini.animate cursor
-			{
-				"echasnovski/mini.animate",
-				optional = true,
-				opts = {
-					cursor = { enable = false },
-				},
-			},
-		},
+	"sphamba/smear-cursor.nvim",
+	opts = {
+		hide_target_hack = true,
+		cursor_color = "none",
 	},
 }
