70e7b94b38c3f384d71092ffb9325b30e6bd82d6
Collie Tsai <colliemst@gmail.com>
2025-12-02 15:24:02 +0800

    fix: fish wrapped command cannot parse options

--- home/dot_config/fish/functions/lal.fish
+++ home/dot_config/fish/functions/lal.fish
@@ -1,7 +1,7 @@
 function lal --description 'eza -Alh --icons --git (fallback to ls)' --wraps eza
     if command -q eza
-        command eza --icons --git -Alh --total-size --group-directories-first -- $argv
+        command eza --icons --git -Alh --total-size --group-directories-first $argv
     else
-        command ls -Alh -- $argv
+        command ls -Alh $argv
     end
 end
--- home/dot_config/fish/functions/ll.fish
+++ home/dot_config/fish/functions/ll.fish
@@ -1,7 +1,7 @@
 function ll --description 'eza -lh --icons --git (fallback to ls)' --wraps eza
-  if command -q eza
-    command eza --icons --git --group-directories-first -lh --total-size -- $argv
-  else
-    command ls -lh -- $argv
-  end
+    if command -q eza
+        command eza --icons --git --group-directories-first -lh --total-size $argv
+    else
+        command ls -lh $argv
+    end
 end
