2978a5b7cb2f3da65d1628ead9aaf51779e9bf83
Collie Tsai <colliemst@gmail.com>
2026-04-14 15:16:45 +0800

    add gio

--- home/dot_config/fish/conf.d/abbr.fish
+++ home/dot_config/fish/conf.d/abbr.fish
@@ -6,6 +6,10 @@ abbr -a glg "git log"
 abbr -a gp "git push"
 abbr -a gst "git status"
 abbr -a rd rmdir
+abbr -a trash 'gio trash'
+abbr -a tl 'gio trash --list'
+abbr -a tre 'gio trash --restore'
+abbr -a te 'gio trash --empty'
 abbr -a sdn "shutdown now"
 abbr -a tosync "cd ~/syncfolder/"
 abbr -a ... "cd ../.."
